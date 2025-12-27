805cbbd6471a1634ae9c11686ff5751f78288dc3
Hardilpreet Kaur <hardilpreetkaur2@gmail.com>
2025-12-28 00:44:19 +0530

    Implement basic calculator operations

---
+++ calculator.py
@@ -0,0 +1,18 @@
+num1= flaot(input("Enter first number:"))
+num2= flaot(input("Enter second number:"))
+
+#Sum
+print("SUM:",num1+num2)
+
+#Difference
+print("Difference:",num1-num2)
+
+#Product
+print("Product:",num1*num2)
+
+#Division
+if num2 !=0:
+  print("Quotient:", num1/num2)
+else:
+  print("Cannot divide by zero")
+
